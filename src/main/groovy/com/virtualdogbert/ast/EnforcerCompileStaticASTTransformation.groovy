c48148f8e74c534cc2edead7c6b1c32c38c93d5a
Software Engineering  Geek <virtualdogbert@gmail.com>
2018-05-28 21:55:02 -0400

    Creating Enforcer friendly versions of transactional, compile static, and both mixed.

---
+++ src/main/groovy/com/virtualdogbert/ast/EnforcerCompileStaticASTTransformation.groovy
@@ -0,0 +1,55 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ * Some of the setup is derived from the following grails plugings(Appache Licence)
+ * https://github.com/groovy/groovy-core/blob/4993b10737881b2491c2daa01526fb15dd889ac5/src/main/org/codehaus/groovy/transform/NewifyASTTransformation.java
+ * https://github.com/grails-plugins/grails-redis/tree/master/src/main/groovy/grails/plugins/redis
+ */
+
+package com.virtualdogbert.ast
+
+import groovy.transform.CompileStatic
+import org.codehaus.groovy.ast.MethodNode
+import org.codehaus.groovy.ast.expr.Expression
+import org.codehaus.groovy.control.CompilePhase
+import org.codehaus.groovy.control.SourceUnit
+import org.codehaus.groovy.transform.AbstractASTTransformation
+import org.codehaus.groovy.transform.GroovyASTTransformation
+/**
+ * The annotation enforce takes up to 3 closures can injects a call to the enforce method of the enforcerService at the
+ * beginning of the method.
+ *
+ * This can be applied to a method or a class, but the method will take precedence.
+ *
+ * The first closure is value, just so that the transform can be called without naming the parameter.
+ * If your specifying two or more closures you will have to specify there names in the annotation call.
+ * Examples:
+ * @Enforce ( { true } )
+ * @Enforce ( value = { true } , failure = { println " nice " } )
+ * @Enforce ( value = { true } , failure = { println " nice " } , success = { println " not nice " } )
+ * @Enforce ( value = { false } , failure = { println " not nice " } , success = { println " nice " } )
+ *
+ */
+@CompileStatic
+@GroovyASTTransformation(phase = CompilePhase.SEMANTIC_ANALYSIS)
+class EnforcerCompileStaticASTTransformation extends AbstractASTTransformation implements EnforceTrait {
+
+    void additionalMethodProcessing(SourceUnit source, MethodNode renamedMethodNode, List<Expression> params, Map<String, Expression> members) {
+        compileMethodStatically(source, renamedMethodNode, members)
+    }
+}
