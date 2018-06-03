4a32b53bbae683a43f1d942813ef9c37759842c7
Software Engineering  Geek <virtualdogbert@gmail.com>
2018-06-03 18:05:00 -0400

    Cleaning up build file.

--- src/main/groovy/enforcer/EnforcerGrailsPlugin.groovy
+++ src/main/groovy/enforcer/EnforcerGrailsPlugin.groovy
@@ -3,14 +3,14 @@ package enforcer
 import grails.plugins.*
 
 class EnforcerGrailsPlugin extends Plugin {
-    def grailsVersion = "3.3.1 > *"
-    def title = "Enforcer"
-    def author = "Tucker J. Pelletier AKA virtualdogbert"
+    def grailsVersion = '3.3.1 > *'
+    def title = 'Enforcer'
+    def author = 'Tucker J. Pelletier AKA virtualdogbert'
     def description = 'A plugin for enforcing business rules/permissions, that works with Spring Security Core, is easier to implement, and extend. It can also be used as an alternative to Spring Security ACL'
     def profiles = ['web']
-    def documentation = "https://virtualdogbert.github.io/Enforcer"
-    def license = "APACHE"
-    def developers = [ [ name: "Tucker J. Pelletier"]]
-    def issueManagement = [ system: "GITHUB", url: "https://github.com/virtualdogbert/Enforcer/issues" ]
-    def scm = [ url: "https://github.com/virtualdogbert/Enforcer" ]
+    def documentation = 'https://virtualdogbert.github.io/Enforcer'
+    def license = 'APACHE'
+    def developers = [ [ name: 'Tucker J. Pelletier']]
+    def issueManagement = [ system: 'GITHUB', url: 'https://github.com/virtualdogbert/Enforcer/issues' ]
+    def scm = [ url: 'https://github.com/virtualdogbert/Enforcer' ]
 }
